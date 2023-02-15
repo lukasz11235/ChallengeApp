3d5d6e5f64444bcc082d977f9f906b7f93b80184
lukasz11235 <l11235ukasz1@gmail.com>
2023-02-15 22:15:00 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var myName1 = "Ewa";
+var myAge1 = 33;
+bool female = true;
+if (myName1 == "Ewa" && myAge1 == 33)
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+else if (myAge1 < 30 && female)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+else
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
